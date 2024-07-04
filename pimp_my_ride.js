0ba97a4ed15a6dc348a4b465db863d702bfb16e8
Soléna Toussaint <solena_ananda@hotmail.com>
2024-07-04 14:36:26 +0200

    new .py

--- pimp_my_ride.js
+++ pimp_my_ride.js
@@ -13,8 +13,6 @@ function parseTrip(trip) {
     return prospect;
 }
 
-//parseTrip(tripToParse);
-
 /* Etape 2 : Utilisez la fonction parseTrip(trip) dans une autre fonction parseTrips(trips) prenant en entrée une journée 
 complète (donc plusieurs lignes) et retournant une liste de structures trips définies précédemment  ([]). */
 
@@ -26,11 +24,7 @@ let tripsToParse = [
 ];
 
 function parseTrips(trips) {
-    const prospects = [];
-    for (trip of trips) {
-        prospects.push(parseTrip(trip));
-    }
-    return prospects;
+    return trips.map(parseTrip);
 }
 
 /* Etape 3 :Pour optimiser vos journées, vous décidez d'écrire un code calculant l'enchaînement 
@@ -42,53 +36,41 @@ function getTripsPrice(trips) {
     let sumPrices = 0;
     for (trip of trips) {
         sumPrices += trip.price;
-        //console.log(typeof sumPrices, sumPrices);
     }
     return sumPrices;
 }
-
-//getTripsPrice(parseTrips(tripsToParse));
 
 /* Etape 4 : Créez une fonction `checkCompatibility(tripA, tripB)` qui comparent deux structures `voyages` et 
 retourne un booléen déterminant si les structures sont compatibles ou non.
 Il s'agit de déterminer si un vol (représenté par une structure `trips`) n'empiète pas sur les horaires d'un autre. */
 
 function checkCompatibility(tripA, tripB) {
-    //tripA = parseTrip(tripA);
-    //tripB = parseTrip(tripB);
     let compatibility = false;
-    if (tripA.start + tripA.duration <= tripB.start) {
+    if (
+        tripA.start + tripA.duration <= tripB.start ||
+        tripB.start + tripB.duration <= tripA.start
+    ) {
         compatibility = true;
     }
-    //console.log(compatibility);
     return compatibility;
 }
-
-//checkCompatibility("Roger 2 3 10", "Pongo 5 1 14");
 
 /* Etape 5 : Développez une fonction findCompatibilities(trips) qui retourne, à partir d'une liste de voyages, tous les 
 ensembles de voyages compatibles les uns avec les autres. */
 
 function findCompatibilities(trips) {
-    let tripObjects = parseTrips(trips);
-    //console.log(tripObjects[1])
-    let compatibilityArray = []; //est-ce qu'il y a quelque chose à faire pour éviter de stocker deux fois la même variable ?
-    for (let i = 0; i < tripObjects.length; i++) {
-        compatibilityArray.push([tripObjects[i]]);
-        for (let j = 0; j < tripObjects.length; j++) {
-            if (checkCompatibility(tripObjects[i], tripObjects[j])) {
-                let tempArray = [];
-                tempArray.push(tripObjects[i], tripObjects[j]);
-                compatibilityArray.push(tempArray);
+    let compatibilityArray = [];
+    for (let i = 0; i < trips.length; i++) {
+        compatibilityArray.push([trips[i]]);
+        for (let j = i+1; j < trips.length; j++) {
+            if (checkCompatibility(trips[i], trips[j])) {
+                compatibilityArray.push([trips[i], trips[j]]);
             }
         }
     }
-    //compatibilityArray.push(trip);
-    //console.log(compatibilityArray);
+    console.log("Compatibilities are :", compatibilityArray);
     return compatibilityArray;
 }
-
-findCompatibilities(tripsToParse);
 
 /* Etape 6 : Développez une dernière fonction findBestPrice(trips), qui renverra le combo ou le voyage
  seul qui rapportera le plus à votre entreprise. */
@@ -104,8 +86,9 @@ function findBestPrice(trips) {
             bestCombo = compatibilities[i];
         }
     }
-    console.log(bestCombo);
+    console.log("Best Combo is:", bestCombo);
     return bestCombo;
 }
 
-findBestPrice(tripsToParse);
+let tripsWellParsed = parseTrips(tripsToParse);
+findBestPrice(tripsWellParsed);
---
+++ pimp_my_ride.py
@@ -0,0 +1,77 @@
+trip_to_parse = "Perdita 8 10 8"
+
+
+def parse_trip(trip: str) -> dict:
+    prospect = {}
+    trip = trip.split()
+    prospect["client"] = trip[0]
+    prospect["start"] = int(trip[1])
+    prospect["duration"] = int(trip[2])
+    prospect["price"] = int(trip[3])
+    return prospect
+
+# print(parse_trip(trip_to_parse))
+
+
+trips_to_Parse = [
+    "Roger 0 5 10",
+    "Pongo 3 7 14",
+    "Perdita 8 10 8",
+    "Anita 16 3 7",
+]
+
+
+def parse_trips(trips: list) -> list:
+    return list(map(parse_trip, trips))
+
+# print(parse_trips(trips_to_Parse))
+
+
+def get_trip_prices(trips: list) -> int:
+    sum_prices = 0
+    for trip in trips:
+        sum_prices += trip["price"]
+    return sum_prices
+
+# print(get_trip_prices(parse_trips(trips_to_Parse)))
+
+
+def check_compatibility(trip_a: dict, trip_b: dict) -> bool:
+    compatibility = False
+    if (trip_a["start"] + trip_a["duration"] <= trip_b["start"]
+            or trip_b["start"] + trip_b["duration"] <= trip_a["start"]):
+        compatibility = True
+    return compatibility
+
+# print(check_compatibility({"client": 'Pongo', "start": 3, "duration": 7, "price": 14},
+#                                        {"client": 'Anita', "start": 16, "duration": 3, "price": 7}))
+
+
+def find_compatibilities(trips: list) -> list:
+    compatibility_array = []
+    for x in range(len(trips)):
+        compatibility_array.append([trips[x]])
+        for y in range(x+1, len(trips)):
+            if check_compatibility(trips[x], trips[y]):
+                compatibility_array.append([trips[x], trips[y]])
+    return compatibility_array
+
+# print(find_compatibilities(parse_trips(trips_to_Parse)))
+
+
+def find_best_price(trips: list) -> list:
+    compatibilities = find_compatibilities(trips)
+    max_price = 0
+    best_combo = []
+    for x in range(len(compatibilities)):
+        x_price = get_trip_prices(compatibilities[x])
+        if x_price > max_price:
+            max_price = x_price
+            best_combo = compatibilities[x]
+    return best_combo
+
+
+trips_well_parsed = parse_trips(trips_to_Parse)
+best_capitalist_combo = find_best_price(trips_well_parsed)
+
+print("We're gonna make money with: ", best_capitalist_combo)
